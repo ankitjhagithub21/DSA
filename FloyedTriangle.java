810c0476744a684caff6cd4e6110bdee2bf91a7f
Ankit Jha <91364014+ankitjhagithub21@users.noreply.github.com>
2025-01-07 01:49:53 +0530

    Create FloyedTriangle.java

---
+++ FloyedTriangle.java
@@ -0,0 +1,27 @@
+/*
+1
+2 3
+4 5 6
+7 8 9 10 
+
+*/
+
+class Main {
+    public static void main(String[] args) {
+        int m = 4;
+        int counter = 1;
+        for(int i=1;i<=m;i++){
+           for(int j=1;j<=i;j++){
+                System.out.print(counter+" ");
+                counter++;
+        }
+       
+        System.out.println();
+    }
+        
+       
+    }
+}
+
+
+
