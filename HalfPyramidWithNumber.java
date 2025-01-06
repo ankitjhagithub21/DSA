83b79177f060d5d86e5ac8fb2ef1cbc8de0a2246
Ankit Jha <91364014+ankitjhagithub21@users.noreply.github.com>
2025-01-07 01:40:26 +0530

    Create HalfPyramidWithNumber.java

---
+++ HalfPyramidWithNumber.java
@@ -0,0 +1,28 @@
+/*
+
+1
+12
+123
+1234
+12345
+
+*/
+
+class Main {
+    public static void main(String[] args) {
+        int m = 5;
+        
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=i;j++){
+                System.out.print(j);
+        }
+       
+        System.out.println();
+    }
+        
+       
+    }
+}
+
+
+
